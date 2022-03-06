73e084c188fe380da89a72891569865722f2cc34
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2022-03-06 22:57:44 +0200

    added author resource

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function store(Request $request)
+    {
+        Author::create($request->only([
+            'name', 'dob'
+        ]));
+    }
+}
