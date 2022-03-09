c39f11008afefc0e8aa038ceee630bd19abda35b
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2022-03-09 21:27:41 +0200

    adding checkout and checkin books test

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -23,4 +23,34 @@ class Book extends Model
         ]);
         $this->attributes["author_id"] = $author->id; 
     }
+
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()
+            ->where("user_id", $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if($reservation == null) {
+            throw new \Exception();
+        }
+        
+        $reservation->update([
+            'checked_in_at' => now()
+        ]);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
